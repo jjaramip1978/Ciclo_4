e167ffee48bbd8067e9927c57f4ad5cc447b07a0
Wijuva <wjuradov@correo.udistrital.edu.co>
2021-11-07 19:20:43 -0500

    Se creo la pagina Home del proyecto
    Se agrego en la carpeta Full stack, junto con el backend. La pagina es responsive y esta realizada con bootstrap y React js

---
+++ Full Stack/Frontend/src/components/contact.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const initialState = {
+  name: '',
+  email: '',
+  message: '',
+}
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setState((prevState) => ({ ...prevState, [name]: value }))
+  }
+  const clearState = () => setState({ ...initialState })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email, message)
+    emailjs
+      .sendForm(
+        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          clearState()
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div id='footerStyle'>
+      <div className='container text-center'>
+        <p id='footerStyle-content p'>
+          &copy; 2021 MisionTIC 2022, Universidad Tecnologica de Pereira, Pereira, Colombia.
+        </p>
+      </div>
+    </div>
+  )
+}
